aa0880cdd3a75d46d2827118d313ef8f9e882abd
Kristina Shalaginova <kristinashalaginova@Kristinas-MBP.ht.home>
2022-03-15 16:01:54 -0400

    adding expressions

---
+++ Expressions in Python.py
@@ -0,0 +1,19 @@
+print(2+5)
+print(2.0+5)
+print (20*2)
+print(2**2)
+print(2/4)
+print(23/5)
+print(23//5)
+print(25%4)
+a = 2 + 5
+b = 3.3 * 3
+c = 10.0 / 25
+
+print(a)
+print(b)
+print(c)
+print(25*15+33/2.0)
+a = 25*15
+b = 33/2.0
+print(a+b)

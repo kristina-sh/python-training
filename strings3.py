2df047dfa0fc15ac409a77aedfe5af6d592cbcd0
Kristina Shalaginova <kristinashalaginova@Kristinas-MBP.ht.home>
2022-03-16 15:48:02 -0400

    Adding strings

---
+++ strings3.py
@@ -0,0 +1,34 @@
+#strings
+
+string = 'I am a string in Python'
+string1 ="I am a string in Python"
+
+string[0]
+
+len(string)
+print(string[-2])
+
+len(string)
+print(len(string))
+
+#I am a string in Python
+
+print(string)
+
+string = 'I am string in Python'
+print(string[5:11])
+print(string[-6:])
+
+string2 = 'Con' + 'catenation'
+print(string2)
+
+string3 = 2 * ('Con' + 'catenation')
+print(string3)
+
+word = "Ford"
+word = 'L' +word[1:]
+print(word)
+
+print("Today I had {0} cups of {1}".format(3,"coffee"))
+print('prices: ({x}, {y}, {z})'.format(x = 2.0, y = 1.5, z = 5))
+

0bb5fd3bad2a051d29a5b19186699aa545424121
Kristina Shalaginova <kristinashalaginova@Kristinas-MBP.ht.home>
2022-03-17 16:00:08 -0400

    Adding if else branching

---
+++ branching.py
@@ -0,0 +1,43 @@
+print(5 < 6)
+print(10 > 11)
+print(4 != 1)
+print('abc' == 'abc')
+a = True
+b = False
+print(not a)
+print(a and b)
+print (a or a)
+d = 5
+e = 1
+f = False
+g = 'python'
+h = 'some'
+z = not((not(e <=d) and (g >= h)) or f) and 1
+print(z)
+
+
+
+passerby_speech = ''
+if passerby_speech == 'hello' or passerby_speech == 'hi':
+  print("Hi, how are you?")
+else:
+    print('Hey')
+
+
+
+passerby_speech = 'Hi'
+if passerby_speech == 'Hello':
+  print("Hi")
+else:
+    print('Hey')
+
+
+    passerby_speech = 'Hi'
+    if passerby_speech == 'Hello':
+        print("Hi")
+    elif passerby_speech == "Hi":
+        print("Hello")
+    else:
+        print("Hey")
+        
+
